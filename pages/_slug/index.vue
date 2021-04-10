3460a61f8cd8833c9dbf811f872c33be59ec473d
SuguruOoki <oki.work0527@gmail.com>
2021-04-11 00:23:36 +0900

    Add post detail page script logic

---
+++ pages/_slug/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ title }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <div class="post" v-html="body"></div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ $config: { apiBaseUrl, xApiKey }, params }) {
+    const { data } = await axios.get(
+      `${apiBaseUrl}/blog/${params.slug}`,
+      {
+        headers: { 'X-API-KEY': xApiKey }
+      }
+    )
+    return data
+  }
+}
+</script>
