69ec24d08354e486af3ba3f006ddd3fe3a1670c5
SuguruOoki <oki.work0527@gmail.com>
2021-04-11 00:32:55 +0900

    Add draft page

---
+++ pages/draft.vue
@@ -0,0 +1,32 @@
+<template>
+  <main v-if="data" class="main">
+    <h1 class="title">{{ data.title }}</h1>
+    <p class="publishedAt">{{ data.publishedAt }}</p>
+    <div class="post" v-html="data.body"></div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      data: {}
+    };
+  },
+  async created({ $config: { apiBaseUrl, xApiKey }}) {
+    const query = this.$route.query;
+    if (query.id === undefined || query.draftKey === undefined) {
+      return;
+    }
+    const { data } = await axios.get(
+      `${apiBaseUrl}/blog/${query.id}?draftKey=${query.draftKey}`,
+      {
+        headers: { 'X-API-KEY': xApiKey }
+      }
+    )
+    this.data = data;
+  },
+}
+</script>
